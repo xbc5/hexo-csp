e40e59d99faacd5faa9ca5244b6ecef5868a9b9d
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-05-28 12:51:08 +0100

    fix: broken inline hashes
      Don't base64 encode the hex, use the raw hash string instead. Fix all
      of the broken tests.
    
    Squashed commit of the following:
    
    commit 9b62de999399fbc90fd265f7a742b788981f147e
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Sat May 28 12:49:12 2022 +0100
    
        check after_render snapshots
    
    commit a1da847e522ea77dfb052d6614f3339fd6019209
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Sat May 28 08:32:07 2022 +0100
    
        update applyCSP snapshots
    
    commit 2d5217dd0662cb6ede8b95ed7913ebd55b3e212a
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 13:35:10 2022 +0100
    
        remove redundant tests and comments from Hash spec
    
    commit 780f1f2349be774d629dd15edcbca8b280b1bfad
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 13:33:50 2022 +0100
    
        restructure test suite layout for Hash
    
    commit ad1eb55073a4f7e5f780d7383c8fd57cbb7e1c71
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 13:30:44 2022 +0100
    
        add more complete test suite for crypto
    
    commit 3a30013200bcfa7244a912850a01e301e8c07534
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 13:17:39 2022 +0100
    
        refactor Hash, remove unnecessary methods, make it simpler
    
    commit da26e676397ca9e5c713810e468ca89e49b4a509
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 13:06:41 2022 +0100
    
        get proper base64 hashing working (initial)
    
    commit 357a9de578d0b2f2bc41b49bab6f72d3b2e8bd60
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 12:41:04 2022 +0100
    
        create a failing test for hash+base64
    
          Because this is the root cause of the inline policy failures.
    
    commit a357b20eba42f7b2d1fbdfdc03425ba6421762f1
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri May 27 07:58:59 2022 +0100
    
        change hash NPM script
    
          Use a known working script specified here: https://content-security-policy.com/hash/

--- __tests__/crypto/Hash.spec.js
+++ __tests__/crypto/Hash.spec.js
@@ -1,17 +1,5 @@
 const { Hash } = require("../../lib/crypto");
 
-it("should throw given an invalid algo", () => {
-  expect(() => new Hash("foo", "invalid-algo")).toThrow();
-});
-
-// sha256: f63082b2a4ff4c064ffb53149db9cd6b89c7cb6c375e1e33108954eb8477a9f8
-//
-// sha384: 9b80e66be5721f7cfcba929efda2eea4a38444d578071d94fb3d207bbd686bf6
-//         a1a0c62706f179d505cb85668bea8abb
-//
-// sha512: 1086692e5d74db16d1cf12ad9cedddd6eb649be0d1525c86c22fcf61c9be2101
-//         6132c23652dd075720fd5cba7981f84b8490815255677c35b89b84f9551b39cd
-//
 // WARN: DO NOT MODIFY THIS STRING
 // tests will compute a hash from it, including spaces and newlines.
 // prettier-ignore-start
@@ -22,110 +10,63 @@ WITH_SPACES = `
     `;
 // prettier-ignore-end
 
-describe("hash()", () => {
-  [
-    {
-      algo: "sha256",
-      input: "foo",
-      expected:
-        "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
-      desc: "should hash a simple string correctly",
-    },
-    {
-      algo: "sha256",
-      input: WITH_SPACES,
-      expected:
-        "f63082b2a4ff4c064ffb53149db9cd6b89c7cb6c375e1e33108954eb8477a9f8",
-      desc: "should hash a string with newlines and spaces correctly",
-    },
-    {
-      algo: "sha256",
-      input: "\n\t\t\n",
-      expected:
-        "2d15ca90d999b495a220b9dcfab9482a552d63ff397d9dfcd6a1285a57b833a9",
-      desc: "should handle newlines and tabs correctly",
-    },
-    {
-      algo: "sha384",
-      input: "foo",
-      expected:
-        "98c11ffdfdd540676b1a137cb1a22b2a70350c9a44171d6b1180c6be5cbb2ee3f79d532c8a1dd9ef2e8e08e752a3babb",
-      desc: "should hash a simple string correctly",
-    },
-    {
-      algo: "sha384",
-      input: WITH_SPACES,
-      expected:
-        "9b80e66be5721f7cfcba929efda2eea4a38444d578071d94fb3d207bbd686bf6a1a0c62706f179d505cb85668bea8abb",
-      desc: "should hash a string with newlines and spaces correctly",
-    },
-    {
-      algo: "sha384",
-      input: "\n\t\t\n",
-      expected:
-        "86d147bf0badb773b7e29eef5871c2ab58c80b6c46cea0c27134ab18e6ae2f9f3a2efdf2b695532c07a04f1a2c093c93",
-      desc: "should handle newlines and tabs correctly",
-    },
-    {
-      algo: "sha512",
-      input: "foo",
-      expected:
-        "f7fbba6e0636f890e56fbbf3283e524c6fa3204ae298382d624741d0dc6638326e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7",
-      desc: "should hash a simple string correctly",
-    },
-    {
-      algo: "sha512",
-      input: WITH_SPACES,
-      expected:
-        "1086692e5d74db16d1cf12ad9cedddd6eb649be0d1525c86c22fcf61c9be21016132c23652dd075720fd5cba7981f84b8490815255677c35b89b84f9551b39cd",
-      desc: "should hash a string with newlines and spaces correctly",
-    },
-    {
-      algo: "sha512",
-      input: "\n\t\t\n",
-      expected:
-        "3bdba7f24f0a521286d984d287df9fa356f0e80e1c054bcd8114067b5831a7aebf6af4714f2f9bd4ad0f1587b2cd42958480f4db5b595fe63a9aaf2161657320",
-      desc: "should handle newlines and tabs correctly",
-    },
-  ].forEach(({ algo, input, expected, desc }) => {
-    describe(`@ ${algo}`, () => {
-      it(desc, () => {
+it("should throw given an invalid algo", () => {
+  expect(() => new Hash("foo", "invalid-algo")).toThrow();
+});
+
+[
+  {
+    input: "foo",
+    expected: "LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=",
+    algo: "sha256",
+  },
+  {
+    input: WITH_SPACES,
+    expected: "9jCCsqT/TAZP+1MUnbnNa4nHy2w3Xh4zEIlU64R3qfg=",
+    algo: "sha256",
+    inputDesc: "a CSS class with spaces",
+  },
+  {
+    // this one is lifted from: https://content-security-policy.com/hash/, and is
+    // therefore known to work.
+    input: "doSomething();",
+    expected: "RFWPLDbv2BY+rCkDzsE+0fr8ylGr2R2faWMhq4lfEQc=",
+    algo: "sha256",
+  },
+  {
+    input: "foo",
+    expected:
+      "mMEf/f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7",
+    algo: "sha384",
+  },
+  {
+    input: WITH_SPACES,
+    expected:
+      "m4Dma+VyH3z8upKe/aLupKOERNV4Bx2U+z0ge71oa/ahoMYnBvF51QXLhWaL6oq7",
+    algo: "sha384",
+    inputDesc: "a CSS class with spaces",
+  },
+  {
+    input: "foo",
+    expected:
+      "9/u6bgY2+JDlb7vzKD5STG+jIErimDgtYkdB0NxmODJuKCxBvl5CVNiCB3LFUYosWowMf37aGVlKfrU5RT4e1w==",
+    algo: "sha512",
+  },
+  {
+    input: WITH_SPACES,
+    expected:
+      "EIZpLl102xbRzxKtnO3d1utkm+DRUlyGwi/PYcm+IQFhMsI2Ut0HVyD9XLp5gfhLhJCBUlVnfDW4m4T5VRs5zQ==",
+    algo: "sha512",
+    inputDesc: "a CSS class with spaces",
+  },
+].forEach(({ input, expected, algo, inputDesc }) => {
+  const inputMsg = inputDesc ? `<<${inputDesc}>>` : input;
+  describe(`for hash.generate()`, () => {
+    describe(`when algo="${algo}"`, () => {
+      it(`should correctly hash+base64 when input="${inputMsg}"`, () => {
         const hash = new Hash(input, algo);
-        expect(hash.hash().toString()).toBe(expected);
+        expect(hash.generate()).toBe(expected);
       });
     });
-  });
-});
-
-describe("base64()", () => {
-  it("should base64 encode the subject", () => {
-    const hash = new Hash("foo");
-    const expected = "Zm9v";
-    expect(hash.base64().toString()).toBe(expected);
-  });
-});
-
-describe("toString() only", () => {
-  it("should return the subject untouched", () => {
-    const hash = new Hash("foo");
-    expect(hash.toString()).toBe("foo");
-  });
-});
-
-describe("hash().base64()", () => {
-  it("should return the hashed + base64 subject", () => {
-    const hash = new Hash("foo", "sha256");
-    expected =
-      "MmMyNmI0NmI2OGZmYzY4ZmY5OWI0NTNjMWQzMDQxMzQxMzQyMmQ3MDY0ODNiZmEwZjk4YTVlODg2MjY2ZTdhZQ==";
-    expect(hash.hash().base64().toString()).toBe(expected);
-  });
-});
-
-describe("generate()", () => {
-  it("should return a fully hashed and encoded value", () => {
-    const hash = new Hash("foo", "sha256");
-    expected =
-      "MmMyNmI0NmI2OGZmYzY4ZmY5OWI0NTNjMWQzMDQxMzQxMzQyMmQ3MDY0ODNiZmEwZjk4YTVlODg2MjY2ZTdhZQ==";
-    expect(hash.generate()).toBe(expected);
   });
 });
